7198aed29367afb81d97d67d28d2a016793d3ea1
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-04-24 11:50:37 -0300

    Treminei a atividade 03, deu trabalho

--- js/atividade3.js
+++ js/atividade3.js
@@ -2,9 +2,19 @@ const botao = document.getElementById("botao")
 const nome = document.getElementById("nome")
 const ul = document.getElementById("ul")
 
-botao.addEventListener("click", function(){
-   
+botao.addEventListener('click', function() {
+    const valorDoInput = nome.value;
 
+    if (valorDoInput.trim() !== '') {
+        
+        const novoItem = document.createElement('li');
+        novoItem.textContent = valorDoInput;
+
+        ul.appendChild(novoItem);
+
+        nome.value = '';
+        nome.focus(); 
+    }
 
 
 })
