d39259c8911dbd462065f8cf72f1e42d6538b12f
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-04-23 22:12:23 -0300

    Terminando atividade 03, adicionar lista.

---
+++ js/atividade3.js
@@ -0,0 +1,10 @@
+const botao = document.getElementById("botao")
+const nome = document.getElementById("nome")
+const ul = document.getElementById("ul")
+
+botao.addEventListener("click", function(){
+   
+
+
+
+})
